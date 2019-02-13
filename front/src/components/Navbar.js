31196576aec8df9a94376b9333ba739b420e1930
Jonathan <jonglbrt@gmail.com>
2019-02-12 22:33:47 -0500

    Worked on the Navbar, added CSS, added links to Index

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
+import '../css/style.css'
+import { Link } from 'react-router-dom'
+// import { connect } from 'react-redux'
 
-function Navbar () {
-  return (
-    <div />
-  )
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-main-style">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            Movie Lists
+          </Link>
+
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/signup" className="nav-link">
+                  CREATE ACCOUNT
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">
+                  LOGIN
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/lists" className="nav-link">
+                  LISTS
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/reviews" className="nav-link">
+                  REVIEWS
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/reviews" className="nav-link">
+                  CHAT
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default Navbar
