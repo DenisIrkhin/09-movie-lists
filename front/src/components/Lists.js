cef1b8d08ab111348662ecb8cebe7053805b16aa
konradobritzhauser <konradobritz@gmail.com>
2019-02-17 12:47:37 -0500

    finish login and signup

---
+++ front/src/components/Lists.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "../css/style.css";
+import {Redirect,Link } from "react-router-dom";
+import "../css/LoginSignup.css";
+import { connect } from "react-redux";
+import axios from "axios";
+import App from "../App.js"
+import Modal from "react-modal"
+
+class Lists extends Component{
+
+    render(){
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+export default Lists
