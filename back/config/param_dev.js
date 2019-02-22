3394ae2468a37f6bc0d363b518eb9ba08a2b44ab
Denis Irkhin <jscriptor5@gmail.com>
2019-02-21 21:32:13 -0500

    Switch to our proper mongodb instance on DO

--- back/config/param_dev.js
+++ back/config/param_dev.js
@@ -1,5 +1,12 @@
 module.exports = {
-  mongoURL: 'mongodb+srv://dbUser:jdTMDbkXmPiTGfFm8oJUXkN@cluster0-xywd0.mongodb.net/test?retryWrites=true',
+  MONGO_ATLAS_AWS_36: 'mongodb+srv://dbUser:jdTMDbkXmPiTGfFm8oJUXkN@cluster0-xywd0.mongodb.net/test?retryWrites=true',
+  MONGO_ATLAS_AWS_34: 'mongodb://dbUser:jdTMDbkXmPiTGfFm8oJUXkN@cluster0-shard-00-00-xywd0.mongodb.net:27017,cluster0-shard-00-01-xywd0.mongodb.net:27017,cluster0-shard-00-02-xywd0.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  MONGO_ATLAS_GOOGLE_36: 'mongodb+srv://dbUserGoogle:VrNmqrJWckunJcdN8VRZXecTR@cluster0-tcrmo.gcp.mongodb.net/test?retryWrites=true',
+  MONGO_ATLAS_GOOGLE_34: 'mongodb://dbUserGoogle:VrNmqrJWckunJcdN8VRZXecTR@cluster0-shard-00-00-tcrmo.gcp.mongodb.net:27017,cluster0-shard-00-01-tcrmo.gcp.mongodb.net:27017,cluster0-shard-00-02-tcrmo.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  MONGO_ATLAS_AZURE_36: 'mongodb+srv://dbUserAzure:VrNmqrJWckunJcdN8VRZXecTR@cluster0-xyuti.azure.mongodb.net/test?retryWrites=true',
+  MONGO_ATLAS_AZURE_34: 'mongodb://dbUserAzure:jenpETZyxiTTNRuKdHT8hFiyZ@cluster0-shard-00-00-xyuti.azure.mongodb.net:27017,cluster0-shard-00-01-xyuti.azure.mongodb.net:27017,cluster0-shard-00-02-xyuti.azure.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
+  MONGO_MLAB: 'mongodb://dd:QWhHQrWztwZ8netQMY@ds129045.mlab.com:29045/ml',
+  MONGO_DO: 'mongodb://dbUser:TsTQNyBwykyjBegXv8PaUUWtu@movielists.ca:27017/ml',
   CORS: 'http://localhost:3080',
   SERVER_PORT: 5050
 }
--- back/mongo-dbo-promise.js
+++ back/mongo-dbo-promise.js
@@ -1,12 +1,22 @@
 const MongoClient = require('mongodb').MongoClient
 
 // MLab import from separate file for security reason also.
-const url = require('./config/param').mongoURL
-// console.log('url', url)
+// AWS does NOT work for any version
+// const uri = require('./config/param').MONGO_ATLAS_AWS_36
+
+// Google the most stable of three even with v3.6+
+// const uri = require('./config/param').MONGO_ATLAS_GOOGLE_34
+
+// Works fine
+// const uri = require('./config/param').MONGO_ATLAS_AZURE_34
+// Does NOT work stable
+// const uri = require('./config/param').MONGO_ATLAS_AZURE_36
+
+const uri = require('./config/param').MONGO_DO
+console.log('Mongo', uri)
 
 let gdbo = new Promise((resolve, reject) => {
-  MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
-    //   //   debugger
+  MongoClient.connect(uri, { useNewUrlParser: true }, (err, db) => {
     if (err) reject(err)
     resolve(db.db('ml'))
     //   // console.log('dbo is ', dbo)
