01f9aa4c3755ccf69083c73e24b9dc885752a519
Jonathan <jonglbrt@gmail.com>
2019-02-25 19:39:31 -0500

    Added styling for Tags in My Lists

--- front/src/components/Lists.js
+++ front/src/components/Lists.js
@@ -1,87 +1,86 @@
-import React, { Component } from "react";
-import "../css/style.css";
-import { Redirect, Link } from "react-router-dom";
-import "../css/LoginSignup.css";
-import { connect } from "react-redux";
-import axios from "axios";
-import App from "../App.js";
-import { withRouter } from "react-router";
-import Modal from "react-modal";
-import { isThisQuarter } from "date-fns";
-import { EditLists } from "./EditList";
-import { FacebookShareButton, TwitterShareButton } from "react-share";
+import React, { Component } from 'react'
+import '../css/style.css'
+import { Redirect, Link } from 'react-router-dom'
+import '../css/LoginSignup.css'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import App from '../App.js'
+import { withRouter } from 'react-router'
+import Modal from 'react-modal'
+import { isThisQuarter } from 'date-fns'
+import { EditLists } from './EditList'
+import { FacebookShareButton, TwitterShareButton } from 'react-share'
 
 class UnconnectedLists extends Component {
   constructor(props) {
-    super(props);
-    this.state={uniqueTags:[]}
-    this.displayLists = this.displayLists.bind(this);
-    this.deleteList = this.deleteList.bind(this);
-    this.editList = this.editList.bind(this);
-    this.setDisplayedTags = this.setDisplayedTags.bind(this);
-    this.displayTags=this.displayTags.bind(this)
-
+    super(props)
+    this.state = { uniqueTags: [] }
+    this.displayLists = this.displayLists.bind(this)
+    this.deleteList = this.deleteList.bind(this)
+    this.editList = this.editList.bind(this)
+    this.setDisplayedTags = this.setDisplayedTags.bind(this)
+    this.displayTags = this.displayTags.bind(this)
   }
-  
+
   componentDidMount() {
-    console.log("fetched to get list");
-    console.log("url: /api/lists");
+    console.log('fetched to get list')
+    console.log('url: /api/lists')
     axios({
-      method: "get",
-      url: "/api/lists",
+      method: 'get',
+      url: '/api/lists',
       withCredentials: true
     }).then(response => {
-      console.log("response", response);
-      let responseLists = response.data.lists;
-      console.log("responseLists", responseLists);
-      this.props.dispatch({ type: "getLists", payload: responseLists });
+      console.log('response', response)
+      let responseLists = response.data.lists
+      console.log('responseLists', responseLists)
+      this.props.dispatch({ type: 'getLists', payload: responseLists })
       this.setDisplayedTags()
-    });
+    })
   }
 
   fetchList() {
-    let that=this
-    console.log("fetched to get list");
-    console.log("url: /api/lists");
+    let that = this
+    console.log('fetched to get list')
+    console.log('url: /api/lists')
     axios({
-      method: "get",
-      url: "/api/lists",
+      method: 'get',
+      url: '/api/lists',
       withCredentials: true
     }).then(response => {
-      console.log("response", response);
-      let responseLists = response.data.lists;
-      console.log("responseLists", responseLists);
-      this.props.dispatch({ type: "getLists", payload: responseLists });
+      console.log('response', response)
+      let responseLists = response.data.lists
+      console.log('responseLists', responseLists)
+      this.props.dispatch({ type: 'getLists', payload: responseLists })
       that.setDisplayedTags()
     })
   }
 
   editList(list) {
-    this.props.dispatch({ type: "editList", payload: list });
-    this.props.history.push("./lists/editlist");
+    this.props.dispatch({ type: 'editList', payload: list })
+    this.props.history.push('./lists/editlist')
   }
 
   deleteList(listId) {
-    let that = this;
-    console.log("listId", listId);
-    console.log("fetch request /api/lists/delete");
+    let that = this
+    console.log('listId', listId)
+    console.log('fetch request /api/lists/delete')
     axios({
-      method: "delete",
-      url: "/api/lists/id",
+      method: 'delete',
+      url: '/api/lists/id',
       data: { listId: listId },
       withCredentials: true
     }).then(response => {
-      console.log("response", response);
-      console.log("response.message", response.message);
-      that.fetchList();
-    });
+      console.log('response', response)
+      console.log('response.message', response.message)
+      that.fetchList()
+    })
   }
 
   displayLists() {
-    let that = this;
+    let that = this
     try {
-      let listsArr = this.props.lists;
-      console.log("listsArr", listsArr);
+      let listsArr = this.props.lists
+      console.log('listsArr', listsArr)
 
       function createListElements(elem, index) {
         // console.log('elem', elem)
@@ -89,23 +88,23 @@ class UnconnectedLists extends Component {
 
         return (
           <li className="each-list">
-            <Link to={"lists/" + elem._id}>
+            <Link to={'lists/' + elem._id}>
               <span className="title-lists">{elem.name}</span>
             </Link>
             <span className="lists-icons">
               <FacebookShareButton
-                url={window.location.origin + "/lists/" + elem._id}
-                className={"fab fa-facebook ml-1 mr-1 icon-lists"}
+                url={window.location.origin + '/lists/' + elem._id}
+                className={'fab fa-facebook ml-1 mr-1 icon-lists'}
               />
               <TwitterShareButton
-                url={window.location.origin + "/lists/" + elem._id}
-                className={"fab fa-twitter-square ml-1 mr-1 icon-lists"}
+                url={window.location.origin + '/lists/' + elem._id}
+                className={'fab fa-twitter-square ml-1 mr-1 icon-lists'}
               />
               <span
                 name="edit"
                 className="far fa-edit MouseOver ml-1 mr-1 icon-lists"
                 onClick={() => {
-                  that.editList(elem);
+                  that.editList(elem)
                 }}
               />
               <span
@@ -115,7 +114,7 @@ class UnconnectedLists extends Component {
               />
             </span>
           </li>
-        );
+        )
       }
       if (!listsArr.length) {
         return (
@@ -123,80 +122,87 @@ class UnconnectedLists extends Component {
             <h4>No Lists have been created yet</h4>
             <Link to="/lists/makelist">Create a List</Link>
           </div>
-        );
+        )
       } else {
-        console.log("listsArr to be mapped", listsArr);
-        return listsArr.map(createListElements);
+        console.log('listsArr to be mapped', listsArr)
+        return listsArr.map(createListElements)
       }
     } catch {}
   }
 
-  displayTags(){
-
-    
-    let renderDomElement=(elem)=>{
-      return <Link to={"./searchtags/"+elem}><span><span>{elem}</span><span className="fas fa-tag"></span></span></Link>
+  displayTags() {
+    let renderDomElement = elem => {
+      return (
+        <Link to={'./searchtags/' + elem}>
+          <span className="lists-tag ml-1 mr-1">
+            {elem} <span className="fas fa-tag" />
+          </span>
+        </Link>
+      )
     }
     return this.state.uniqueTags.map(renderDomElement)
   }
   setDisplayedTags() {
     if (true) {
-      console.log("displaying all tags in the lists.....");
+      console.log('displaying all tags in the lists.....')
       let getListTagsArr = list => {
         // console.log("specific list tags",list.tags.split(" ^^ "))
-        return list.tags.split(" ^^ ");
-      };
+        return list.tags.split(' ^^ ')
+      }
       let getArrAllTags = lists => {
-        console.log("called ArrAllTags");
-        console.log("lists", lists);
-        let ret = [];
+        console.log('called ArrAllTags')
+        console.log('lists', lists)
+        let ret = []
         lists.forEach(list => {
           // console.log('list', list)
           // console.log('ret before', ret)
-          ret = ret.concat(getListTagsArr(list));
+          ret = ret.concat(getListTagsArr(list))
           // console.log('retAfter', ret)
-        });
+        })
         // console.log('ret', ret)
-        return ret;
-      };
-      let ArrAllTags = getArrAllTags(this.props.lists);
-      console.log("ArrAllTags", ArrAllTags);
-      let tagSet = new Set(ArrAllTags);
-      console.log("tagSet", tagSet);
-      let uniqueTagsArr = Array.from(tagSet);
-      console.log("uniqueTagsArr", uniqueTagsArr);
+        return ret
+      }
+      let ArrAllTags = getArrAllTags(this.props.lists)
+      console.log('ArrAllTags', ArrAllTags)
+      let tagSet = new Set(ArrAllTags)
+      console.log('tagSet', tagSet)
+      let uniqueTagsArr = Array.from(tagSet)
+      console.log('uniqueTagsArr', uniqueTagsArr)
 
-      this.setState({uniqueTags:uniqueTagsArr})
+      this.setState({ uniqueTags: uniqueTagsArr })
     }
   }
 
   render() {
     if (!this.props.loggedIn) {
-      return <Redirect to="/loginalert" />;
+      return <Redirect to="/loginalert" />
     } else {
-      return (<div className="row">
-        <div className="container-fluid main-container-lists">
-          <h2>Your Lists</h2>
-          <div className="container">
-            <div className="row">
-              <ul className="lists-holder">{this.displayLists()}</ul>
+      return (
+        <div className="row">
+          <div className="container-fluid main-container-lists">
+            <div className="container">
+              <div className="row">
+                <ul className="col-md-8 lists-holder">
+                  <h4 className="text-center mb-3">Your Lists</h4>
+                  {this.displayLists()}
+                </ul>
+
+                <div className="col-md-3 tags-holder-lists">
+                  <h4 className="text-center mb-3">Your Tags</h4>
+                  {this.displayTags()}
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div>
-          <h2>Your Tags</h2>
-          {this.displayTags()}
-        </div>
-        </div>
-
-      );
+      )
     }
   }
 }
 
 let mapStateToProps = function(state) {
-  return { lists: state.state.lists, loggedIn: state.state.loggedIn };
-};
-let Lists = connect(mapStateToProps)(withRouter(UnconnectedLists));
+  return { lists: state.state.lists, loggedIn: state.state.loggedIn }
+}
+let Lists = connect(mapStateToProps)(withRouter(UnconnectedLists))
 
-export default Lists;
+export default Lists
