00c3c4cd88d9e924654a4d62c8a412bc0875b2b7
Jonathan <jonglbrt@gmail.com>
2019-02-27 16:21:30 -0500

    More changes

--- front/src/components/LoginAlert.js
+++ front/src/components/LoginAlert.js
@@ -1,35 +1,33 @@
-import React, { Component } from "react";
-import "../css/style.css";
-import { Redirect, Link } from "react-router-dom";
+import React, { Component } from 'react'
+import '../css/style.css'
+import { Redirect, Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
-import "../css/MakeList.css";
-import { connect } from "react-redux";
-import axios from "axios";
-import App from "../App.js";
-import Modal from "react-modal";
+import '../css/MakeList.css'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import App from '../App.js'
+import Modal from 'react-modal'
 
-
-class UnconnectedLoginAlert extends Component{
-
-
-    render(){
-        if(this.props.loggedIn){
-            this.props.history.push("/")
-            return(<div></div>)
-        }else{
-
-        return(
-            <h3 className="jump" style={{position:"relative",top:"50px"}}>To access this feature please sign up or Log in</h3>
-        )}
+class UnconnectedLoginAlert extends Component {
+  render() {
+    if (this.props.loggedIn) {
+      this.props.history.push('/')
+      return <div />
+    } else {
+      return (
+        <div className="container-fluid main-container-my-reviews">
+          <h3 className="jump" style={{ position: 'relative', top: '50px' }}>
+            To access this feature please Sign up or Log in
+          </h3>
+        </div>
+      )
     }
+  }
 }
 
-
-
-
-let mapStateToProps=function(state){
-    return {loggedIn:state.state.loggedIn}
+let mapStateToProps = function(state) {
+  return { loggedIn: state.state.loggedIn }
 }
 
-let LoginAlert=connect(mapStateToProps)(withRouter(UnconnectedLoginAlert))
+let LoginAlert = connect(mapStateToProps)(withRouter(UnconnectedLoginAlert))
 export default LoginAlert
